e3564e05ba9353fa9685d8b9a91285448a8811db
CoderLambert <156486648@qq.com>
2018-03-27 00:30:31 +0800

    完成分页功能，下一步实现搜索和支持markdown功能

--- use_ckeditor/views.py
+++ use_ckeditor/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render,get_object_or_404
 from .models import *
 from django.http import Http404
+
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 def HomePage(request):
-    articals = Article.objects.all()
-    return render(request, 'index.html',{'articals':articals})
+
+    all_articals = Article.objects.all()
+    artical_nums = all_articals.count()
+
+    try:
+        page = request.GET.get('page', 1)
+    except PageNotAnInteger:
+        page = 1
+    p = Paginator(all_articals, per_page=5, request=request)
+    articals = p.page(page)
+    return render(request, 'index.html',{
+        'all_articals':articals,
+    #'articals':articals,
+        'artical_nums':artical_nums})
 
 def ArticleInfo(request,artical_id):
     try:
