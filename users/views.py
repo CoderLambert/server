829aa38bffe562b2332433cd229bcf8660b24604
CoderLambert <156486648@qq.com>
2018-04-18 20:40:44 +0800

    添加用户名不存在时的错误消息

--- users/views.py
+++ users/views.py
@@ -32,17 +32,12 @@ def Userlogin(request):
            #第二步 login，向request里面写东西，然后返回到render里面
            login(request,user)
            #正常应该返回到首页
-           all_articals = Article.objects.all()
-           artical_nums = all_articals.count()
-           web_list = Web_link.objects.all()
+
            return render(request, 'index.html',
                          {
-                             'all_articals': all_articals,
-                             'artical_nums': artical_nums,
-                             'web_list': web_list
                          }
                          )
        else:
-           return render(request, 'login.html', {})
+           return render(request, 'login.html', {"error_msg":"没有此用户，请检查用户名或密码"})
    elif request.method == "GET":
        return render(request,'login.html',{})
